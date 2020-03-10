d1150b7f5203c7fbc86228906e558b9ce0677181
anuj-modi <anuj1modi@gmail.com>
2020-03-09 22:36:25 -0400

    convert submitted 2 and 3

--- tests/submitted_test_2.cpp
+++ tests/submitted_test_2.cpp
@@ -1,54 +1,24 @@
-// #include <gtest/gtest.h>
-// #include "dataframe.h"
+#include "../src/dataframe.h"
+#include "catch.hpp"
 
-// #define GT_TRUE(a) ASSERT_EQ((a), true)
-// #define GT_FALSE(a) ASSERT_EQ((a), false)
-// #define GT_EQUALS(a, b) ASSERT_EQ(a, b)
-// #define ASSERT_EXIT_ZERO(a) ASSERT_EXIT(a(), ::testing::ExitedWithCode(0), ".*")
+/** Tests that adding a column to the dataframe adds a copy of it,
+ * since it was mentioned on Piazza that it should be a copy
+ */
+TEST_CASE("Add column to dataframe is copy", "[column][dataframe]") {
+    Schema s("S");
+    DataFrame df(s);
+    Row r = Row(df.get_schema());
+    String* str = new String("Test");
+    r.set(0, str);
+    df.add_row(r);
+    IntColumn* col = new IntColumn();
+    col->push_back(4);
+    String* col_name = new String("Names");
+    df.add_column(col, col_name);
+    col->as_int()->set(0, 8);
 
-// /**
-//  * Determine if these two floats are equal with respect to eps.
-//  * @param f1 the first float to compare.
-//  * @param f2 the second float to compare.
-//  */
-// bool float_equal(float f1, float f2) {
-//     float eps = 0.0000001;
-//     if (f1 > f2) {
-//         return f1 - f2 < eps;
-//     } else {
-//         return f2 - f1 < eps;
-//     }
-// }
+    REQUIRE(df.get_int(1, 0) == 4);
 
-// /** Tests that adding a column to the dataframe adds a copy of it,
-//  * since it was mentioned on Piazza that it should be a copy
-//  */
-// void test() {
-//     Schema s("S");
-//     DataFrame df(s);
-//     Row r = Row(df.get_schema());
-//     String* str = new String("Test");
-//     r.set(0, str);
-//     df.add_row(r);
-//     IntColumn* col = new IntColumn();
-//     col->push_back(4);
-//     String* col_name = new String("Names");
-//     df.add_column(col, col_name);
-//     col->as_int()->set(0, 8);
-
-//     GT_EQUALS(df.get_int(1, 0), 4);
-
-//     delete str;
-//     delete col_name;
-
-//     exit(0);
-// }
-
-// TEST(a4, t1) {
-//     ASSERT_EXIT_ZERO(test);
-// }
-
-// int main(int argc, char** argv) {
-//     testing::InitGoogleTest(&argc, argv);
-//     return RUN_ALL_TESTS();
-// }
+    delete str;
+    delete col_name;
+}
--- tests/submitted_test_3.cpp
+++ tests/submitted_test_3.cpp
@@ -1,60 +1,28 @@
-// #include <gtest/gtest.h>
-// #include "dataframe.h"
+#include "../src/dataframe.h"
+#include "catch.hpp"
 
-// #define GT_TRUE(a) ASSERT_EQ((a), true)
-// #define GT_FALSE(a) ASSERT_EQ((a), false)
-// #define GT_EQUALS(a, b) ASSERT_EQ(a, b)
-// #define ASSERT_EXIT_ZERO(a) ASSERT_EXIT(a(), ::testing::ExitedWithCode(0), ".*")
+/** Tests that adding a column with a length not equal to the
+ * df length will not add the column or will add it with padding.
+ */
+TEST_CASE("uneven column length", "[schema][dataframe]") {
+    Schema s("S");
+    DataFrame df(s);
+    Row r = Row(df.get_schema());
+    String* str = new String("Test");
+    r.set(0, str);
+    df.add_row(r);
+    IntColumn* col = new IntColumn();
+    String* col_name = new String("Names");
+    df.add_column(col, col_name);
 
-// /**
-//  * Determine if these two floats are equal with respect to eps.
-//  * @param f1 the first float to compare.
-//  * @param f2 the second float to compare.
-//  */
-// bool float_equal(float f1, float f2) {
-//     float eps = 0.0000001;
-//     if (f1 > f2) {
-//         return f1 - f2 < eps;
-//     } else {
-//         return f2 - f1 < eps;
-//     }
-// }
+    if (df.ncols() == 1) {
+        REQUIRE(true);
+    } else {
+        df.set(1, 0, 11);
+        REQUIRE((df.ncols() == 2 && df.get_int(1, 0) == 11));
+    }
 
-// /** Tests that adding a column with a length not equal to the
-//  * df length will not add the column or will add it with padding.
-//  */
-// void test() {
-//     bool first_opt = false;
-//     bool second_opt = false;
-//     Schema s("S");
-//     DataFrame df(s);
-//     Row r = Row(df.get_schema());
-//     String* str = new String("Test");
-//     r.set(0, str);
-//     df.add_row(r);
-//     IntColumn* col = new IntColumn();
-//     String* col_name = new String("Names");
-//     df.add_column(col, col_name);
-
-//     if (df.ncols() == 1) {
-//         GT_TRUE(true);
-//     } else {
-//         df.set(1, 0, 11);
-//         GT_TRUE(df.ncols() == 2 && df.get_int(1, 0) == 11);
-//     }
-
-//     delete str;
-//     delete col;
-//     delete col_name;
-
-//     exit(0);
-// }
-
-// TEST(a4, t1) {
-//     ASSERT_EXIT_ZERO(test);
-// }
-
-// int main(int argc, char** argv) {
-//     testing::InitGoogleTest(&argc, argv);
-//     return RUN_ALL_TESTS();
-// }
+    delete str;
+    delete col;
+    delete col_name;
+}
