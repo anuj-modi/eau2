f1a23c515357048d06ed20fc7252181c7f338a6e
unknown <19chrisg97@gmail.com>
2020-03-10 16:50:34 -0400

    plugged memory errors

--- src/array.h
+++ src/array.h
@@ -936,7 +936,7 @@ class DoubleArray {
         vals_ = new double[capacity_];
     }
 
-    DoubleArray(Deserializer* d) : DoubleArray() {
+    DoubleArray(Deserializer* d) {
         size_ = d->get_size_t();
         capacity_ = d->get_size_t();
         vals_ = (double*)d->get_buffer(size_ * sizeof(double));
