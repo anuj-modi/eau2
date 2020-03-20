d39f0c31ce2cb30b12328acd57bffdfd3a4a63ff
chris-gomes <19chrisg97@gmail.com>
2020-03-19 22:42:49 -0400

    updated makefile to not recompile last test file

--- tests/value_test.cpp
+++ tests/value_test.cpp
@@ -6,9 +6,9 @@
 TEST_CASE("equal for value", "[value]") {
     String s("serialized data");
     String s_2("different serialized data");
-    Value v_1(s.c_str());
-    Value* same = new Value(s.c_str());
-    Value* different = new Value(s_2.c_str());
+    Value v_1(s.c_str(), s.size());
+    Value* same = new Value(s.c_str(), s.size());
+    Value* different = new Value(s_2.c_str(), s_2.size());
 
     REQUIRE(v_1.equals(same));
     REQUIRE_FALSE(v_1.equals(different));
@@ -21,9 +21,9 @@ TEST_CASE("equal for value", "[value]") {
 TEST_CASE("hash a value", "[value]") {
     String s("serialized data");
     String s_2("different serialized data");
-    Value v_1(s.c_str());
-    Value* same = new Value(s.c_str());
-    Value* different = new Value(s_2.c_str());
+    Value v_1(s.c_str(), s.size());
+    Value* same = new Value(s.c_str(), s.size());
+    Value* different = new Value(s_2.c_str(), s_2.size());
 
     REQUIRE(v_1.hash() == same->hash());
     REQUIRE_FALSE(v_1.hash() == different->hash());
