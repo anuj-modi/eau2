0b21b9972e4a392db9c4e1cf7b6fe45e95a0ac86
chris-gomes <19chrisg97@gmail.com>
2020-03-27 00:20:53 -0400

    updated row

--- src/dataframe/row.h
+++ src/dataframe/row.h
@@ -4,6 +4,7 @@
 #include "util/array.h"
 #include "util/object.h"
 #include "util/string.h"
+#include <vector>
 
 /*************************************************************************
  * Row::
@@ -16,30 +17,30 @@
 class Row : public Object {
    public:
     int index_ = -1;
-    Array* values_;
-    IntArray* types_;
+    std::vector<Object*> values_;
+    std::vector<char> types_;
 
     /** Build a row following a schema. */
     Row(Schema& scm) : Object() {
-        values_ = new Array();
-        types_ = new IntArray();
+        values_ = std::vector<Object*>();
+        types_ = std::vector<int>();
         for (size_t i = 0; i < scm.width(); i++) {
             switch (scm.col_type(i)) {
                 case 'S':
-                    values_->push_back(new StringColumn());
-                    types_->push_back(type_to_int_('S'));
+                    values_.push_back(new StringArray());
+                    types_.push_back('S');
                     break;
                 case 'B':
-                    values_->push_back(new BoolColumn());
-                    types_->push_back(type_to_int_('B'));
+                    values_.push_back(new BoolArray());
+                    types_.push_back('B');
                     break;
                 case 'I':
-                    values_->push_back(new IntColumn());
-                    types_->push_back(type_to_int_('I'));
+                    values_.push_back(new IntArray());
+                    types_.push_back('I');
                     break;
                 case 'D':
-                    values_->push_back(new DoubleColumn());
-                    types_->push_back(type_to_int_('D'));
+                    values_.push_back(new DoubleArray());
+                    types_.push_back('D');
                     break;
                 default:
                     assert(false);
@@ -48,19 +49,17 @@ class Row : public Object {
     }
 
     virtual ~Row() {
-        for (size_t i = 0; i < values_->size(); i++) {
-            delete values_->get(i);
+        for (size_t i = 0; i < values_.size(); i++) {
+            delete values_[i];
         }
-        delete values_;
-        delete types_;
     }
 
     /** Setters: set the given column with the given value. Setting a column with
      * a value of the wrong type is undefined. */
     void set(size_t col, int val) {
-        assert(col < values_->size());
-        assert(type_to_char_(types_->get(col)) == 'I');
-        IntColumn* c = static_cast<Column*>(values_->get(col))->as_int();
+        assert(col < values_.size());
+        assert(types_[col] == 'I');
+        IntArray* c = static_cast<IntArray*>(values_[col]);
         assert(c != nullptr);
         if (c->size() == 0) {
             c->push_back(val);
@@ -69,9 +68,10 @@ class Row : public Object {
         }
     }
     void set(size_t col, double val) {
-        assert(col < values_->size());
-        assert(type_to_char_(types_->get(col)) == 'D');
-        DoubleColumn* c = static_cast<Column*>(values_->get(col))->as_double();
+        assert(col < values_.size());
+        assert(types_[col] == 'D');
+        DoubleArray* c = static_cast<DoubleArray*>(values_[col]);
+        assert(c != nullptr);
         if (c->size() == 0) {
             c->push_back(val);
         } else {
@@ -79,9 +79,10 @@ class Row : public Object {
         }
     }
     void set(size_t col, bool val) {
-        assert(col < values_->size());
-        assert(type_to_char_(types_->get(col)) == 'B');
-        BoolColumn* c = static_cast<Column*>(values_->get(col))->as_bool();
+        assert(col < values_.size());
+        assert(types_[col] == 'B');
+        BoolArray* c = static_cast<BoolArray*>(values_[col]);
+        assert(c != nullptr);
         if (c->size() == 0) {
             c->push_back(val);
         } else {
@@ -91,9 +92,10 @@ class Row : public Object {
 
     /** The string is external. */
     void set(size_t col, String* val) {
-        assert(col < values_->size());
-        assert(type_to_char_(types_->get(col)) == 'S');
-        StringColumn* c = static_cast<Column*>(values_->get(col))->as_string();
+        assert(col < values_.size());
+        assert(types_[col] == 'S');
+        StringArray* c = static_cast<StringArray*>(values_[col]);
+        assert(c != nullptr);
         if (c->size() == 0) {
             c->push_back(val);
         } else {
@@ -113,45 +115,45 @@ class Row : public Object {
     /** Getters: get the value at the given column. If the column is not
      * of the requested type, the result is undefined. */
     int get_int(size_t col) {
-        assert(col < values_->size());
-        assert(type_to_char_(types_->get(col)) == 'I');
-        return static_cast<Column*>(values_->get(col))->as_int()->get(0);
+        assert(col < values_.size());
+        assert(types_[col] == 'I');
+        return static_cast<IntArray*>(values_[col])->get(0);
     }
 
     bool get_bool(size_t col) {
-        assert(col < values_->size());
-        assert(type_to_char_(types_->get(col)) == 'B');
-        return static_cast<Column*>(values_->get(col))->as_bool()->get(0);
+        assert(col < values_.size());
+        assert(types_[col] == 'B');
+        return static_cast<BoolArray*>(values_[col])->get(0);
     }
 
     double get_double(size_t col) {
-        assert(col < values_->size());
-        assert(type_to_char_(types_->get(col)) == 'D');
-        return static_cast<Column*>(values_->get(col))->as_double()->get(0);
+        assert(col < values_.size());
+        assert(types_[col] == 'D');
+        return static_cast<DoubleArray*>(values_[col])->get(0);
     }
 
     String* get_string(size_t col) {
-        assert(col < values_->size());
-        assert(type_to_char_(types_->get(col)) == 'S');
-        return static_cast<Column*>(values_->get(col))->as_string()->get(0);
+        assert(col < values_.size());
+        assert(types_[col] == 'S');
+        return static_cast<StringArray*>(values_[col])->get(0);
     }
 
     /** Number of fields in the row. */
     size_t width() {
-        return types_->size();
+        return types_.size();
     }
 
     /** Type of the field at the given position. An idx >= width is  undefined. */
     char col_type(size_t idx) {
-        assert(idx < types_->size());
-        return type_to_char_(types_->get(idx));
+        assert(idx < types_.size());
+        return types_[idx];
     }
 
     /** Given a Fielder, visit every field of this row. The first argument is
      * index of the row in the dataframe.
      * Calling this method before the row's fields have been set is undefined. */
     void visit(size_t idx, Fielder& f) {
-        assert(idx < values_->size());
+        assert(idx < values_.size());
         switch (col_type(idx)) {
             case 'S':
                 f.accept(get_string(idx));
@@ -166,38 +168,6 @@ class Row : public Object {
                 f.accept(get_bool(idx));
                 break;
             default:
-                assert(false);
-        }
-    }
-
-    int type_to_int_(char c) {
-        switch (c) {
-            case 'S':
-                return 1;
-            case 'B':
-                return 2;
-            case 'I':
-                return 3;
-            case 'D':
-                return 4;
-            default:
-                printf("Invalid column type given");
-                assert(false);
-        }
-    }
-
-    char type_to_char_(int i) {
-        switch (i) {
-            case 1:
-                return 'S';
-            case 2:
-                return 'B';
-            case 3:
-                return 'I';
-            case 4:
-                return 'D';
-            default:
-                printf("Invalid column type given");
                 assert(false);
         }
     }
