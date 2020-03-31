c492d5e0e562cf7d52d4813d0d3b03960c54bae8
chris-gomes <19chrisg97@gmail.com>
2020-03-31 18:40:02 -0400

    row setup

--- src/dataframe/row.h
+++ src/dataframe/row.h
@@ -86,7 +86,7 @@ class Row : public Object {
     /** Given a Fielder, visit every field of this row. The first argument is
      * index of the row in the dataframe.
      * Calling this method before the row's fields have been set is undefined. */
-    void visit(size_t idx, Visitor& f) {
+    void visit(size_t idx, Visitor& v) {
         assert(idx < values_.size());
         switch (col_type(idx)) {
             case 'S':
