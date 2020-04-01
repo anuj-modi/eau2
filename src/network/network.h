1c3bd0d041f5965ce5a357ac0ebfcb08336a9dd7
anuj-modi <anuj1modi@gmail.com>
2020-03-31 23:51:11 -0400

    switch to listen and connection socket objects

--- src/network/network.h
+++ src/network/network.h
@@ -6,6 +6,7 @@
 #include <sys/select.h>
 #include <sys/socket.h>
 #include <unistd.h>
+
 #include "util/object.h"
 #include "util/string.h"
 
@@ -98,28 +99,21 @@ class Address : public Object {
 class Socket : public Object {
    public:
     int sock_fd_;
-    bool is_bound_sock_;
     bool is_closed_;
-    Address *peer_addr_;
 
     Socket() : Object() {
         sock_fd_ = socket(AF_INET, SOCK_STREAM, 0);
         assert(sock_fd_ != -1);
-        is_bound_sock_ = false;
-        peer_addr_ = nullptr;
         is_closed_ = false;
     }
 
-    Socket(int socket_fd, Address *peer_address) : Object() {
+    Socket(int socket_fd) : Object() {
         sock_fd_ = socket_fd;
         is_closed_ = false;
-        peer_addr_ = peer_address;
-        is_bound_sock_ = false;
     }
 
     virtual ~Socket() {
         close_socket();
-        delete peer_addr_;
     }
 
     /**
@@ -129,18 +123,7 @@ class Socket : public Object {
      * @param port a port number to bind to
      */
     void bind_and_listen(const char *ip, uint16_t port) {
-        int opt = 1;
-        // Forcefully attaching socket to the port 8080
-        assert(setsockopt(sock_fd_, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)) ==
-               0);
-        struct sockaddr_in adr;
-        adr.sin_family = AF_INET;
-        inet_pton(AF_INET, ip, &adr.sin_addr);
-        adr.sin_port = htons(port);
-        // Attaching socket to the port
-        assert(bind(sock_fd_, (struct sockaddr *)&adr, sizeof(adr)) == 0);
-        assert(listen(sock_fd_, 3) >= 0);
-        is_bound_sock_ = true;
+        assert(false);
     }
 
     /**
@@ -149,26 +132,16 @@ class Socket : public Object {
      * @param port the port of the remote server to connect to
      */
     void connect_to_other(const char *ip, uint16_t port) {
-        assert(!is_bound_sock_);
-        peer_addr_ = new Address(ip);
-        struct sockaddr_in adr;
-        adr.sin_family = AF_INET;
-        adr.sin_addr.s_addr = peer_addr_->as_bytes();
-        adr.sin_port = htons(port);
-        assert(connect(sock_fd_, (struct sockaddr *)&adr, sizeof(adr)) == 0);
+        assert(false);
     }
 
     /**
      * Accept a new connection for this socket. Blocks if there are no waiting connections.
      * @returns a new Socket object specific to the new connection
      */
-    Socket *accept_connection() {
-        assert(is_bound_sock_);
-        struct sockaddr_in addr;
-        socklen_t size = sizeof(addr);
-        int new_connection_fd = accept(sock_fd_, (struct sockaddr *)&addr, &size);
-        assert(new_connection_fd != -1);
-        return new Socket(new_connection_fd, new Address(addr));
+    ConnectionSocket *accept_connection() {
+        assert(false);
+        return nullptr;
     }
 
     /**
@@ -188,8 +161,8 @@ class Socket : public Object {
      * @returns true if there are new connections waiting, false otherwise.
      */
     bool has_new_connections() {
-        assert(is_bound_sock_);
-        return is_ready_();
+        assert(false);
+        return false;
     }
 
     /**
@@ -197,7 +170,113 @@ class Socket : public Object {
      * @returns true if there are new bytes on the socket, false otherwise.
      */
     bool has_new_bytes() {
-        assert(!is_bound_sock_);
+        assert(false);
+        return false;
+    }
+
+    /**
+     * Read bytes from the socket into the provided buffer.
+     * @param buffer the buffer to place the read bytes into
+     * @param num_bytes the number of bytes to read from the socket
+     * @returns the number of bytes actually read from the socket
+     */
+    size_t recv_bytes(char *buffer, size_t num_bytes) {
+        assert(false);
+        return 0;
+    }
+
+    /**
+     * Write bytes from the provided buffer into the socket to send to the remote host.
+     * @param buffer the buffer to containing bytes to be written.
+     * @param num_bytes the number of bytes to write from the buffer to the socket
+     * @returns the number of bytes actually written to the socket
+     */
+    size_t send_bytes(const char *buffer, size_t num_bytes) {
+        assert(false);
+        return 0;
+    }
+
+    void close_socket() {
+        if (!is_closed_) {
+            close(sock_fd_);
+        }
+    }
+};
+
+class ListenSocket : public Socket {
+   public:
+    /**
+     * Binds this socket to an address and port and begins listening for new connections.
+     *
+     * @param ip A string IP address to listen on
+     * @param port a port number to bind to
+     */
+    void bind_and_listen(const char *ip, uint16_t port) {
+        int opt = 1;
+        // Forcefully attaching socket to the port 8080
+        assert(setsockopt(sock_fd_, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)) ==
+               0);
+        struct sockaddr_in adr;
+        adr.sin_family = AF_INET;
+        inet_pton(AF_INET, ip, &adr.sin_addr);
+        adr.sin_port = htons(port);
+        // Attaching socket to the port
+        assert(bind(sock_fd_, (struct sockaddr *)&adr, sizeof(adr)) == 0);
+        assert(listen(sock_fd_, 3) >= 0);
+    }
+
+    /**
+     * Accept a new connection for this socket. Blocks if there are no waiting connections.
+     * @returns a new Socket object specific to the new connection
+     */
+    ConnectionSocket *accept_connection() {
+        struct sockaddr_in addr;
+        socklen_t size = sizeof(addr);
+        int new_connection_fd = accept(sock_fd_, (struct sockaddr *)&addr, &size);
+        assert(new_connection_fd != -1);
+        return new ConnectionSocket(new_connection_fd, new Address(addr));
+    }
+
+    /**
+     * Checks if this listening socket has any new connections.
+     * @returns true if there are new connections waiting, false otherwise.
+     */
+    bool has_new_connections() {
+        return is_ready_();
+    }
+};
+
+class ConnectionSocket : public Socket {
+   public:
+    Address *peer_addr_;
+
+    ConnectionSocket(int socket_fd, Address *peer_address) : Socket(socket_fd) {
+        peer_addr_ = peer_address;
+    }
+
+    virtual ~ConnectionSocket() {
+        delete peer_addr_;
+    }
+
+    /**
+     * Connects this socket to a remote port.
+     * @param ip the remote IP address to connect to
+     * @param port the port of the remote server to connect to
+     */
+    void connect_to_other(const char *ip, uint16_t port) {
+        peer_addr_ = new Address(ip);
+        struct sockaddr_in adr;
+        adr.sin_family = AF_INET;
+        adr.sin_addr.s_addr = peer_addr_->as_bytes();
+        adr.sin_port = htons(port);
+        assert(connect(sock_fd_, (struct sockaddr *)&adr, sizeof(adr)) == 0);
+    }
+
+    /**
+     * Checks if this connection socket has any bytes to be read.
+     * @returns true if there are new bytes on the socket, false otherwise.
+     */
+    bool has_new_bytes() {
         return is_ready_();
     }
 
@@ -208,7 +287,6 @@ class Socket : public Object {
      * @returns the number of bytes actually read from the socket
      */
     size_t recv_bytes(char *buffer, size_t num_bytes) {
-        assert(!is_bound_sock_);
         int bytes_received = recv(sock_fd_, buffer, num_bytes, 0);
         assert(bytes_received >= 0);
         return bytes_received;
@@ -221,15 +299,8 @@ class Socket : public Object {
      * @returns the number of bytes actually written to the socket
      */
     size_t send_bytes(const char *buffer, size_t num_bytes) {
-        assert(!is_bound_sock_);
         int bytes_sent = send(sock_fd_, buffer, num_bytes, 0);
         assert(bytes_sent != -1);
         return bytes_sent;
-    }
-
-    void close_socket() {
-        if (!is_closed_) {
-            close(sock_fd_);
-        }
     }
 };
