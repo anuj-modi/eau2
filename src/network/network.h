f16c3cbfdb977ce880e6c522d2bfbe62161a3856
anuj-modi <anuj1modi@gmail.com>
2020-04-02 22:15:37 +0000

    remove non-shared methdos from BaseSocket

--- src/network/network.h
+++ src/network/network.h
@@ -117,34 +117,6 @@ class BaseSocket : public Object {
     }
 
     /**
-     * Binds this socket to an address and port and begins listening for new connections.
-     *
-     * @param ip A string IP address to listen on
-     * @param port a port number to bind to
-     */
-    void bind_and_listen(const char *ip, uint16_t port) {
-        assert(false);
-    }
-
-    /**
-     * Connects this socket to a remote port.
-     * @param ip the remote IP address to connect to
-     * @param port the port of the remote server to connect to
-     */
-    void connect_to_other(const char *ip, uint16_t port) {
-        assert(false);
-    }
-
-    /**
-     * Accept a new connection for this socket. Blocks if there are no waiting connections.
-     * @returns a new ConnectionSocket object specific to the new connection
-     */
-    ConnectionSocket *accept_connection() {
-        assert(false);
-        return nullptr;
-    }
-
-    /**
      * Polls the underlying socket object for any new activity
      */
     bool is_ready_() {
@@ -156,95 +128,10 @@ class BaseSocket : public Object {
         return result != 0 && pfd.revents == POLLIN;
     }
 
-    /**
-     * Checks if this listening socket has any new connections.
-     * @returns true if there are new connections waiting, false otherwise.
-     */
-    bool has_new_connections() {
-        assert(false);
-        return false;
-    }
-
-    /**
-     * Checks if this connection socket has any bytes to be read.
-     * @returns true if there are new bytes on the socket, false otherwise.
-     */
-    bool has_new_bytes() {
-        assert(false);
-        return false;
-    }
-
-    /**
-     * Read bytes from the socket into the provided buffer.
-     * @param buffer the buffer to place the read bytes into
-     * @param num_bytes the number of bytes to read from the socket
-     * @returns the number of bytes actually read from the socket
-     */
-    size_t recv_bytes(char *buffer, size_t num_bytes) {
-        assert(false);
-        return 0;
-    }
-
-    /**
-     * Write bytes from the provided buffer into the socket to send to the remote host.
-     * @param buffer the buffer to containing bytes to be written.
-     * @param num_bytes the number of bytes to write from the buffer to the socket
-     * @returns the number of bytes actually written to the socket
-     */
-    size_t send_bytes(const char *buffer, size_t num_bytes) {
-        assert(false);
-        return 0;
-    }
-
     void close_socket() {
         if (!is_closed_) {
             close(sock_fd_);
         }
-    }
-};
-
-class ListenSocket : public BaseSocket {
-   public:
-    ListenSocket() : BaseSocket() {}
-
-    /**
-     * Binds this socket to an address and port and begins listening for new connections.
-     *
-     * @param ip A string IP address to listen on
-     * @param port a port number to bind to
-     */
-    void bind_and_listen(const char *ip, uint16_t port) {
-        int opt = 1;
-        // Forcefully attaching socket to the port 8080
-        assert(setsockopt(sock_fd_, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)) ==
-               0);
-        struct sockaddr_in adr;
-        adr.sin_family = AF_INET;
-        inet_pton(AF_INET, ip, &adr.sin_addr);
-        adr.sin_port = htons(port);
-        // Attaching socket to the port
-        assert(bind(sock_fd_, (struct sockaddr *)&adr, sizeof(adr)) == 0);
-        assert(listen(sock_fd_, 3) >= 0);
-    }
-
-    /**
-     * Accept a new connection for this socket. Blocks if there are no waiting connections.
-     * @returns a new ConnectionSocket object specific to the new connection
-     */
-    ConnectionSocket *accept_connection() {
-        struct sockaddr_in addr;
-        socklen_t size = sizeof(addr);
-        int new_connection_fd = accept(sock_fd_, (struct sockaddr *)&addr, &size);
-        assert(new_connection_fd != -1);
-        return new ConnectionSocket(new_connection_fd, new Address(addr));
-    }
-
-    /**
-     * Checks if this listening socket has any new connections.
-     * @returns true if there are new connections waiting, false otherwise.
-     */
-    bool has_new_connections() {
-        return is_ready_();
     }
 };
 
@@ -306,5 +193,50 @@ class ConnectionSocket : public BaseSocket {
         int bytes_sent = send(sock_fd_, buffer, num_bytes, 0);
         assert(bytes_sent != -1);
         return bytes_sent;
+    }
+};
+
+class ListenSocket : public BaseSocket {
+   public:
+    ListenSocket() : BaseSocket() {}
+
+    /**
+     * Binds this socket to an address and port and begins listening for new connections.
+     *
+     * @param ip A string IP address to listen on
+     * @param port a port number to bind to
+     */
+    void bind_and_listen(const char *ip, uint16_t port) {
+        int opt = 1;
+        // Forcefully attaching socket to the port 8080
+        assert(setsockopt(sock_fd_, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)) ==
+               0);
+        struct sockaddr_in adr;
+        adr.sin_family = AF_INET;
+        inet_pton(AF_INET, ip, &adr.sin_addr);
+        adr.sin_port = htons(port);
+        // Attaching socket to the port
+        assert(bind(sock_fd_, (struct sockaddr *)&adr, sizeof(adr)) == 0);
+        assert(listen(sock_fd_, 3) >= 0);
+    }
+
+    /**
+     * Accept a new connection for this socket. Blocks if there are no waiting connections.
+     * @returns a new ConnectionSocket object specific to the new connection
+     */
+    ConnectionSocket *accept_connection() {
+        struct sockaddr_in addr;
+        socklen_t size = sizeof(addr);
+        int new_connection_fd = accept(sock_fd_, (struct sockaddr *)&addr, &size);
+        assert(new_connection_fd != -1);
+        return new ConnectionSocket(new_connection_fd, new Address(addr));
+    }
+
+    /**
+     * Checks if this listening socket has any new connections.
+     * @returns true if there are new connections waiting, false otherwise.
+     */
+    bool has_new_connections() {
+        return is_ready_();
     }
 };
