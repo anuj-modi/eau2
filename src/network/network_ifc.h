1a95a525fa0fd15e32aae4bb2256e11eede3a5ba
chris-gomes <19chrisg97@gmail.com>
2020-04-06 00:22:53 -0400

    updated kdstore tests and added waitAndGet tests

--- src/network/network_ifc.h
+++ src/network/network_ifc.h
@@ -174,6 +174,9 @@ class NetworkIfc : public Thread {
      */
     void handle_directory_message_(Deserializer& d) {
         MsgType m = d.get_msg_type();
+        if (m != MsgType::DIRECTORY) {
+            printf("%d\n", (int)m);
+        }
         assert(m == MsgType::DIRECTORY);
         // create Address array
         Directory dir(&d);
@@ -197,6 +200,7 @@ class NetworkIfc : public Thread {
 
     void connect_to_node_(size_t node) {
         if (connections_.find(node) == connections_.end()) {
+            printf("POTATO");
             assert(peer_addresses_.size() == total_nodes_);
             ConnectionSocket* cs = new ConnectionSocket();
             cs->connect_to_other(peer_addresses_.at(node));
--- tests/kvstore_test.cpp
+++ tests/kvstore_test.cpp
@@ -46,7 +46,35 @@ TEST_CASE("put and get a value in kvstore remote node", "[kvstore]") {
     delete result;
 }
 
-// TODO test kvstore waitAndGet
+// test kvstore waitAndGet
+TEST_CASE("waitAndGet a value in kvstore remote node", "[kvstore]") {
+    String s("sdkfak");
+    Key one = Key("one", 1);
+    Value* v = new Value(s.c_str(), s.size());
+    Address a0("127.0.0.1", 10000);
+    Address a1("127.0.0.1", 10001);
+    NetworkIfc net0(&a0, 2);
+    KVStore kv0(&net0);
+    net0.set_kv(&kv0);
+    NetworkIfc net1(&a1, &a0, 1, 2);
+    KVStore kv1(&net1);
+    net1.set_kv(&kv1);
+
+    net0.start();
+    net1.start();
+
+    kv1.put(one, v);
+    Value* result = kv0.waitAndGet(one);
+
+    REQUIRE(result->equals(v));
+
+    net0.stop();
+    net1.stop();
+    net0.join();
+    net1.join();
+
+    delete result;
+}
 
 // test kvstore this_node
 TEST_CASE("check node number in kvstore", "[kvstore]") {
