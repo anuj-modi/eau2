08e6dae8f7889a1b5377f20e6991dcda552fe73e
anuj-modi <anuj1modi@gmail.com>
2020-04-06 18:34:41 -0400

    comments on network/connection.h

--- src/network/connection.h
+++ src/network/connection.h
@@ -9,8 +9,15 @@
 #include "util/serial.h"
 #include "util/thread.h"
 
+/**
+ * Forward declaration for KVStore due to dependecy issues.
+ */
 class KVStore;
 
+/**
+ * Represents an active connection to another node in the network.
+ * Authors: gomes.chri@husky.neu.edu and modi.an@husky.neu.edu
+ */
 class Connection : public Thread {
    public:
     Lock l_;
@@ -19,20 +26,36 @@ class Connection : public Thread {
     bool keep_processing_;
     std::queue<Reply*> replies;
 
+    /**
+     * Constructs a connection from a socket and a kv store.
+     * @arg s the Socket this connection should use
+     * @arg kv a pointer to the kvstore this connection should use as a datastore
+     */
     Connection(ConnectionSocket* s, KVStore* kv) : Thread(), l_(), replies() {
         s_ = s;
         local_store_ = kv;
         keep_processing_ = true;
     }
 
+    /**
+     * Destructor for a connection.
+     */
     virtual ~Connection() {
         delete s_;
     }
 
+    /**
+     * Stops a connection by ending the processing loop.
+     */
     void stop() {
         keep_processing_ = false;
     }
 
+    /**
+     * Checks if the underlying socket has new bytes.
+     * Uses the locks to ensure thread safety.
+     * @returns true if the socket has new bytes to be read
+     */
     bool socket_ready_() {
         l_.lock();
         bool result = s_->has_new_bytes();
@@ -40,6 +63,11 @@ class Connection : public Thread {
         return result;
     }
 
+    /**
+     * Sends a given message through the connection to the other node.
+     * Uses the lock for thread safety.
+     * @arg m the message to be sent
+     */
     void send_message(Message* m) {
         Serializer s;
         m->serialize(&s);
@@ -50,6 +78,12 @@ class Connection : public Thread {
         l_.unlock();
     }
 
+    /**
+     * Receives a message from the remote host. Will block until some data is received.
+     * Uses the lock for thread safety.
+     * @returns nullptr if 0 bytes were received or a deserializer object containing the received
+     * bytes
+     */
     Deserializer* recv_message_() {
         size_t num_bytes = 0;
         l_.lock();
@@ -73,17 +107,37 @@ class Connection : public Thread {
         printf("%s\n", s.message_->c_str());
     }
 
+    /**
+     * Handles an incoming Get message by quering the KV for the data and sending it back out.
+     * @arg the deserializer containing the get message.
+     */
     void handle_get_message_(Deserializer& d);
 
+    /**
+     * Handles an incoming Put message by calling put on the local KV store object.
+     * @arg d the deserializer object containing the Put message.
+     */
     void handle_put_message_(Deserializer& d);
 
+    /**
+     * Handles an incoming Get message by calling waitAndGet on the local KV and sending the reply
+     * back out.
+     * @arg the deserializer containing the WaitAndGet message.
+     */
     void handle_wait_and_get_message_(Deserializer& d);
 
+    /**
+     * Handles a Reply message by pushing the provided message to the reply queue.
+     * @arg d the deserializer containing the Reply Message.
+     */
     void handle_reply_message_(Deserializer& d) {
         Reply* r = new Reply(&d);
         replies.push(r);
     }
 
+    /** Handles a message by checking the message type and calling the appropriate helper handler.
+     * @arg d the deserializer containing the message bytes
+     */
     void handle_message_(Deserializer& d) {
         MsgType m = d.get_msg_type();
         if (m == MsgType::STATUS) {
@@ -103,7 +157,9 @@ class Connection : public Thread {
         }
     }
 
-    // handles messages from other nodes coming in
+    /**
+     * A processing loop for incoming messages from the peer in the connection.
+     */
     void run() override {
         while (keep_processing_) {
             if (s_->has_new_bytes()) {
